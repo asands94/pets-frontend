fda822b165bd0abe7a264d64037ce6679d126179
Ashley Sands <ashley.sands01@gmail.com>
2024-11-13 13:15:41 -0500

    moved petlist, petdetail, and petform to the dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import './App.css'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
-import * as petService from './services/petService'
-import PetList from './components/PetList'
-import PetDetail from './components/PetDetail'
-import PetForm from './components/PetForm'
 
 import NavBar from './components/NavBar/NavBar'
 import Landing from './components/Landing/Landing'
@@ -12,86 +8,7 @@ import Dashboard from './components/Dashboard/Dashboard'
 import SignupForm from './components/SignupForm/SignupForm'
 
 const App = () => {
-  const [petList, setPetList] = useState([])
-  const [selected, setSelected] = useState(null)
-  const [isFormOpen, setIsFormOpen] = useState(false)
   const [user, setUser] = useState(null)
-
-  useEffect(() => {
-    const fetchPets = async () => {
-      try {
-        const pets = await petService.index()
-
-        if (pets.error) {
-          throw new Error(pets.error)
-        }
-        setPetList(pets)
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    fetchPets()
-  }, [selected])
-
-  const updateSelected = (pet) => {
-    setSelected(pet)
-  }
-
-  const handleFormView = (pet) => {
-    if (!pet.name) setSelected(null)
-    setIsFormOpen(!isFormOpen)
-  }
-
-  const handleAddPet = async (formData) => {
-    try {
-      const newPet = await petService.create(formData)
-      if (newPet.error) {
-        throw new Error(newPet.error)
-      }
-      setPetList([newPet, ...petList])
-      setIsFormOpen(false)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  const handleUpdatePet = async (formData, id) => {
-    try {
-      const updatedPet = await petService.updatePet(formData, id)
-
-      if (updatedPet.error) {
-        throw new Error(updatedPet.error)
-      }
-
-      setPetList((prevState) =>
-        prevState.map((pet) => (pet._id === id ? updatedPet : pet))
-      )
-
-      setIsFormOpen(false)
-      setSelected(updatedPet)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  const handleRemovePet = async (id) => {
-    try {
-      const deletedPet = await petService.deletePet(id)
-
-      if (deletedPet.error) {
-        throw new Error(deletedPet.error)
-      }
-
-      setPetList((prevState) =>
-        prevState.filter((pet) => pet._id !== deletedPet._id)
-      )
-      console.log(petList)
-
-      setSelected(null)
-      setIsFormOpen(false)
-    } catch (error) {
-      console.log(error)
-    }
-  }
 
   return (
     <>
@@ -105,25 +22,6 @@ const App = () => {
 
         <Route path='/signup' element={<SignupForm setUser={setUser} />} />
       </Routes>
-      {/* <PetList
-        petList={petList}
-        updateSelected={updateSelected}
-        handleFormView={handleFormView}
-        isFormOpen={isFormOpen}
-      />
-      {isFormOpen ? (
-        <PetForm
-          handleAddPet={handleAddPet}
-          selected={selected}
-          handleUpdatePet={handleUpdatePet}
-        />
-      ) : (
-        <PetDetail
-          selected={selected}
-          handleFormView={handleFormView}
-          handleRemovePet={handleRemovePet}
-        />
-      )} */}
     </>
   )
 }
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,4 +1,89 @@
+import { useState, useEffect } from 'react'
+import PetDetail from '../PetDetail'
+import PetList from '../PetList'
+import PetForm from '../PetForm'
+import * as petService from '../../services/petService'
+
 const Dashboard = ({ user }) => {
+  const [petList, setPetList] = useState([])
+  const [selected, setSelected] = useState(null)
+  const [isFormOpen, setIsFormOpen] = useState(false)
+
+  useEffect(() => {
+    const fetchPets = async () => {
+      try {
+        const pets = await petService.index()
+
+        if (pets.error) {
+          throw new Error(pets.error)
+        }
+        setPetList(pets)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchPets()
+  }, [selected])
+
+  const updateSelected = (pet) => {
+    setSelected(pet)
+  }
+
+  const handleFormView = (pet) => {
+    if (!pet.name) setSelected(null)
+    setIsFormOpen(!isFormOpen)
+  }
+
+  const handleAddPet = async (formData) => {
+    try {
+      const newPet = await petService.create(formData)
+      if (newPet.error) {
+        throw new Error(newPet.error)
+      }
+      setPetList([newPet, ...petList])
+      setIsFormOpen(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  const handleUpdatePet = async (formData, id) => {
+    try {
+      const updatedPet = await petService.updatePet(formData, id)
+
+      if (updatedPet.error) {
+        throw new Error(updatedPet.error)
+      }
+
+      setPetList((prevState) =>
+        prevState.map((pet) => (pet._id === id ? updatedPet : pet))
+      )
+
+      setIsFormOpen(false)
+      setSelected(updatedPet)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleRemovePet = async (id) => {
+    try {
+      const deletedPet = await petService.deletePet(id)
+
+      if (deletedPet.error) {
+        throw new Error(deletedPet.error)
+      }
+
+      setPetList((prevState) =>
+        prevState.filter((pet) => pet._id !== deletedPet._id)
+      )
+      console.log(petList)
+
+      setSelected(null)
+      setIsFormOpen(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <main>
       <h1>Welcome, {user.username}</h1>
@@ -6,6 +91,25 @@ const Dashboard = ({ user }) => {
         This is the dashboard page where you, and only you, can see a dashboard
         of all of your things.
       </p>
+      <PetList
+        petList={petList}
+        updateSelected={updateSelected}
+        handleFormView={handleFormView}
+        isFormOpen={isFormOpen}
+      />
+      {isFormOpen ? (
+        <PetForm
+          handleAddPet={handleAddPet}
+          selected={selected}
+          handleUpdatePet={handleUpdatePet}
+        />
+      ) : (
+        <PetDetail
+          selected={selected}
+          handleFormView={handleFormView}
+          handleRemovePet={handleRemovePet}
+        />
+      )}
     </main>
   )
 }
